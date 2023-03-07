59b88ddf0ca8bca0d62ba7e8918346ce945b7b12
Anojan Pirabakaran <69903847+anojanpirabakaran@users.noreply.github.com>
2023-03-07 16:08:00 +0100

    add MenuIcon

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import MenuButton from '../../atoms/MenuIcon'
 import './HomePage.css'
 
 export default function HomePage() {
   return (
     <>
+    <MenuButton />
     <div className='home_title'>Welcome to KuracWiki!</div>
     <div className='dir_page_link'>Get your knowledge here</div>
     </>
