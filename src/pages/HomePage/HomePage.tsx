358092b62189ca9a05a22090a4e65821aed2af85
Matijas Polazarov <matijaspolazarov@Matijass-MacBook-Pro.local>
2023-03-21 16:09:22 +0100

    added cards

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './HomePage.css'
+import CustomCard from '../../components/CustomCard'
+import CardPage from './CardPage'
 
 export default function HomePage() {
   return (
     <>
     <div className='home_title'>Welcome to KuracWiki!</div>
     <div className='dir_page_link'>Get your knowledge here</div>
+    
+    <CardPage/>
     </>
   )
 }
